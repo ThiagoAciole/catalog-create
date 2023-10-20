3c361c850c5f9168d383e872b2360ebb65add688
ThiagoAciole <thiagoaciole7@gmail.com>
2023-10-20 10:57:04 -0300

    Divisão de catalogo em paginas de 4 tabelas

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,59 @@ function App() {
   const [title, setTitle] = useState("");
   const [url, setUrl] = useState("");
   const [productsCsv, setProductsCsv] = useState("");
-  const [products, setProducts] = useState([]); // Object to store products
-  
-  const handleAddProduct = () => {
-    const newProducts = productsCsv.split("\n").map((line) => {
-      const [cod, name, emb] = line.split(",");
-      return {
-        cod,
-        name,
-        emb,
-      };
-    });
-    setProducts([...products, newProducts]);
-  
-  };
+  const [products, setProducts] = useState([]);
 
-  const handleGeneratePdf = () => {
-    // Fetch the image from the URL and convert it to base64
+  const handleAddProduct = () => {
     fetch(url)
       .then((response) => response.blob())
       .then((blob) => {
         const reader = new FileReader();
         reader.onload = () => {
           const imageBase64 = reader.result;
-          generatePdf(title, imageBase64, products);
+          const newProducts = productsCsv.split("\n").map((line) => {
+            const [cod, name, emb] = line.split(",");
+            return {
+              title,
+              url: imageBase64,
+              cod,
+              name,
+              emb,
+            };
+          });
+          setProducts([...products, newProducts]);
         };
         reader.readAsDataURL(blob);
       });
   };
 
+ 
+  const handleGeneratePdf = () => {
+    generatePdf(products);
+
+  };
+
+  const handleRemoveProduct = () => {
+    if (products.length > 0) {
+      const newProducts = [...products];
+      newProducts.pop(); 
+      setProducts(newProducts);
+      console.log(products);
+    }
+  };
   return (
     <>
       <div className="flex flex-col space-y-12 p-6">
         <div>
           <h1>Gerador de Catálogo</h1>
         </div>
+
+        {products.length > 0 && (
+          <div className=" flex w-full h-14 items-center rounded-md justify-center space-x-8 bg-blue-200 text-blue-700">
+            <h2><span className="bg-blue-500 p-1  rounded-md text-white"><b>{products.length}</b></span> Produtos adicionados</h2>
+            <button onClick={handleRemoveProduct}>Remove</button>
+          </div>
+        )}
+
         <TextField
           id="title"
           label="Título"
--- src/components/productTable.jsx
+++ src/components/productTable.jsx
@@ -1,21 +1,19 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-extra-semi */
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 
-// Registre as fontes necessárias
-function generatePdf(title, url, productArrays) {
+function generatePdf(data) {
   pdfMake.vfs = pdfFonts.pdfMake.vfs;
-
   function createTable(products) {
     const table = [
       {
-        image: [url],
+        image: products[0].url,
         width: 150,
-        height: 150,
-        margin: [15, 50, 20, 5],
+        margin: [15, 20, 30, 5],
       },
       {
-        text: [title],
+        text: products[0].title,
         style: {
           color: "#727376",
         },
@@ -23,11 +21,16 @@ function generatePdf(title, url, productArrays) {
         bold: true,
         margin: [0, 0, 0, 2],
       },
+      
       {
         table: {
+          
           headerRows: 1,
+          keepWithHeaderRows: 1,
+         
           widths: ["auto", "auto", "auto"],
           body: [
+
             [
               {
                 text: "CÓD.",
@@ -47,22 +50,31 @@ function generatePdf(title, url, productArrays) {
                   color: "#F58634",
                 },
               },
+              
             ],
             ...products.map((p) => [p.cod, p.name, p.emb]),
+            
           ],
+          
         },
         layout: {
           hLineWidth: function (i, node) {
             if (i === 0 || i === node.table.headerRows) {
               return 1; // Adiciona uma linha acima e abaixo do primeiro corpo
             }
-            return i === node.table.body.length ? 1 : 0; // Adiciona uma linha abaixo do último corpo
+            return i === node.table.body.length + node.table.headerRows ? 1 : 0; // Adiciona uma linha abaixo do último corpo
           },
           vLineWidth: function (i) {
             return 0;
           },
           hLineColor: function (i) {
             return i === 0 || i === 1 ? "#3355A3" : "white"; // Cor da linha acima e abaixo do primeiro corpo
+          },
+          fillColor: function (i, node) {
+            return i >= node.table.headerRows &&
+              (i - node.table.headerRows) % 2 === 0
+              ? "#EBECEC"
+              : null; // Cor do fundo zebrado a partir do segundo corpo
           },
           paddingLeft: function (i) {
             return i === 0 ? 0 : 8;
@@ -72,33 +84,81 @@ function generatePdf(title, url, productArrays) {
           },
         },
         width: "*",
+        
         style: {
           fontSize: 7.8,
-          margin: [0, 5, 0, 15],
+          margin: [0, 10, 0, 10],
           color: "#727376",
         },
+        
       },
+      
     ];
     return table;
   }
 
+
   const docDefinition = {
     pageSize: "A4",
-    pageOrientation: "portrait",
-    pageMargins: [40, 40, 40, 40],
-    content: {
-      columns: productArrays.reduce((columns, products) => {
-        if (columns.length === 0 || columns[columns.length - 1].length > 2) {
-          columns.push([createTable(products)]);
-        } else {
-          columns[columns.length - 1].push(createTable(products));
-        }
-        return columns;
-      }, []),
-      columnGap: 20,
-    },
+    content: [],
+    
   };
+  
+  let currentColumns = [];
+  let currentColumn = [];
+  
+  data.forEach((products, index) => {
+    const table = createTable(products);
+  
+    if (currentColumn.length === 2) {
+      currentColumns.push([currentColumn]);
+      currentColumn = [];
+    }
+    currentColumn.push(table);
+    if (index === data.length - 1) {
+      currentColumns.push([currentColumn]);
+    }
+  });
+  
+  // Agrupe as colunas em páginas
+  const maxColumnsPerPage = 2; // Define o número máximo de colunas por página
+  let pageColumns = [];
+  
+  currentColumns.forEach((columns) => {
+    pageColumns.push(columns);
+  
+    if (pageColumns.length === maxColumnsPerPage) {
+      docDefinition.content.push({
+        columns: pageColumns,
+        columnGap: 10,
+        pageBreak: 'after',
+        margin: [10, 10, 10, 10],
+     
+      });
+      
+      pageColumns = [];
+    }
+  });
+  
+  // Adicione as colunas restantes à última página, se houver alguma
+  if (pageColumns.length > 0) {
+    docDefinition.content.push({
+      columns: pageColumns,
+      columnGap: 20,
+    });
+  }
 
+
+
+/*
+  if (pageColumns.length > 0 ) {
+    docDefinition.content.push({
+      columns: pageColumns,
+      columnGap: 20,
+      
+    });
+  }
+*/
   pdfMake.createPdf(docDefinition).open();
 }
 
